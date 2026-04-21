e0ba89f755dfc458a1e54e1a37100ea351bbf474
M Hamza Waqar <126167739+MHamzaWaqar56@users.noreply.github.com>
2026-04-21 10:45:59 +0500

    server changings

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 import contactRoutes from './routes/contactRoutes.js';
